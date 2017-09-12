55770054859a032b5c104e388eb938ec193aae19
duvallwh <whduvall@gmail.com>
2017-09-12 14:49:34 -0700

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -34,9 +35,10 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 		
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock featehrs' for row in rows)
+			any(row.text == '1: Buy peacock featehrs' for row in rows),
+			"New to-do item did not appear in table"
 		)
 		
 		## The box is still there
