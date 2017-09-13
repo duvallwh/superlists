67c28cb7c0282760e8e9dfa2be052d4b0fe63170
duvallwh <whduvall@gmail.com>
2017-09-13 13:43:50 -0700

    Now using django to spin up test server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		## check out apps homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		
 		## title and header mention to-do
 		self.assertIn('To-Do', self.browser.title)
@@ -56,6 +56,3 @@ class NewVisitorTest(unittest.TestCase):
 		
 		## the page updates again, now both items are in the list
 		[...]
-		
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
